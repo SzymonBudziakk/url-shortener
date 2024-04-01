5365f443d92bbc1248694b052d4615ab4332cc68
SzymonBudziakk <szymonbudziakj@gmail.com>
2024-04-01 18:17:18 +0200

    x

--- src/app/[shortUrl]/route.ts
+++ src/app/[shortUrl]/route.ts
@@ -1,22 +1,23 @@
 import { getXataClient } from '@/xata'
-import { auth, useAuth } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs'
 
+// deleting record from db
+// slash in short url?
+// not found page / modal in dashboard
 
 export async function GET(req: Request) {
 
   const {userId} = auth()
-  console.log(userId)
-  
   const shortUrl = req.url
   const xata = getXataClient()
+
   const record = await xata.db.urls
     .filter({ userId: userId, shortUrl: shortUrl })
     .getFirst()
 
   const fullUrl = record?.fullUrl
 
-  // fullUrl you are looking for doesn't exist or in db isnt yours
+  // fullUrl you are looking for doesn't exist in db or isnt yours
   if (!fullUrl) {
     return new Response('', {
       status: 302,
@@ -25,6 +26,11 @@ export async function GET(req: Request) {
       }
     })
   }
+
+
+  const incrementClicks = await xata.db.urls.update(record.id, {
+    clicks: record.clicks + 1,
+  })
 
   return new Response('', {
     status: 302,
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -13,6 +13,7 @@ export default function DataTable({ data }: DataTableProps) {
           <th className='py-3 px-6 text-left'>Original URL</th>
           <th className='py-3 px-6 text-left'>Short URL</th>
           <th className='py-3 px-6 text-left'>Clicks</th>
+          <th className='py-3 px-6 text-left'>Delete</th>
         </tr>
       </thead>
       <tbody className='text-gray-600 text-sm font-light'>
@@ -31,6 +32,38 @@ export default function DataTable({ data }: DataTableProps) {
               </td>
               <td className='py-3 px-6 text-left whitespace-nowrap'>
                 {url.clicks}
+              </td>
+
+              <td>
+                {/* <td className='py-3 px-6 text-left whitespace-nowrap'> */}
+                <button>
+                  {/* <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-6 w-6 fill-blue-600 text-blue-600' // Set fill color to blue-600
+                    viewBox='0 0 24 24'
+                    stroke='#f3f3f3'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M19 7l%-4 4h-3l-4-4m0 0l-4 4h-3l-4-4 4-4'
+                    />
+                  </svg> */}
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-6 w-6 fill-current text-blue-600'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='M12 6v6l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z'
+                    />
+                  </svg>
+                </button>
               </td>
             </tr>
           )
